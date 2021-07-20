d2192f2413b70750ccc628643f21fabd73873811
SMizzz <shinn.mizzz@gmail.com>
2021-07-21 01:03:10 +0900

    Page control Setting

---
+++ CollectionViewPageControl/MyCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionCell.swift
+//  CollectionViewPageControl
+//
+//  Created by 신미지 on 2021/07/21.
+//
+
+import UIKit
+
+class MyCollectionCell: UICollectionViewCell {
+  @IBOutlet weak var myWebImageView: UIImageView!
+}
--- CollectionViewPageControl/ViewController.swift
+++ CollectionViewPageControl/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  var posterImage: [String] = [
+    "furyload",
+    "eightmile",
+    "hiq",
+    "peninsula",
+    "sabaha"
+  ]
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  @IBOutlet weak var myPageControl: UIPageControl!
+  
+//  var currentPage: Int = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    collectionView.delegate = self
+    collectionView.dataSource = self
+    myPageControl.currentPage = 0
+    myPageControl.numberOfPages = posterImage.count
   }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return posterImage.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionCell
+    cell.myWebImageView.image = UIImage(named: posterImage[indexPath.item])
+    cell.myWebImageView.layer.cornerRadius = 50.0
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let width = collectionView.safeAreaLayoutGuide.layoutFrame.width
+    let height = collectionView.safeAreaLayoutGuide.layoutFrame.height
+    return CGSize(width: width, height: height)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    myPageControl.currentPage = indexPath.item
+    
+  }
+}
